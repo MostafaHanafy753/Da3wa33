c2b11d7c1993203043ba1d56ba73404ef264abe2
MostafaHanafy753 <moustafa.hanafy1566@gmail.com>
2023-03-16 01:16:02 +0200

    first commit

---
+++ assets/js/custom/script.js
@@ -0,0 +1,80 @@
+(function ($) {
+  $('.se-vi-carousel').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    rtl: true,
+    prevArrow: '.prev-btn',
+    nextArrow: '.next-btn',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }]
+  });
+  $('.se-vii-carousel').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    rtl: true,
+    prevArrow: '.prev-btn2',
+    nextArrow: '.next-btn2',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }]
+  });
+
+  $('#nav-toggle-btn').on('click', function() {
+    $('#theme-navbar').toggleClass('menu-open');
+  });
+
+  // Plans Toggle Button
+  $('#plansToggleBtn').on('click', function() {
+    if($(this).is(':checked')) {
+      $('.annual-span').hide();
+      $('.month-span').show();
+    } else {
+      $('.annual-span').show();
+      $('.month-span').hide();
+    }
+  });
+
+}(jQuery));
